4dc791281c058da3ae860ea1fcf74af7c7e56343
Dean Gardiner <gardiner91@gmail.com>
2014-03-17 16:58:48 +1300

    Cleaned up 'Code/__init__.py'

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,88 +1,76 @@
-'''
-Plugin entry-point
-'''
 from plugin import SpotifyPlugin, ViewMode
 from settings import PLUGIN_ID, PREFIX, VERSION
-from tornado.ioloop import IOLoop
-from utils import IOLoopProxy
 
 
-''' Globals '''
-runloop = IOLoopProxy(IOLoop.instance())
-plugin = SpotifyPlugin(runloop.ioloop)
+plugin = SpotifyPlugin()
 
 
-def plugin_callback(method, async = False, **kwargs):
-    ''' Invokes callbacks on the plugin instance
-
-    To simplify things we bounce these calls to the reactor thread
-    and wait on a signal for them to return.  This way we don't have to
-    lock things all over the place which would be needed because libspotify
-    is not thread-safe.  By passing everything through this function
-    our entire plugin can be single-threaded.
+def plugin_callback(method, **kwargs):
+    """ Invokes callbacks on the plugin instance
 
     :param method:     The method on the SpotifyPlugin class to call.
-    :param async:      Indicates if the method is asynchronous
-                       (see the documentation for IOLoopProxy)
     :param kwargs:     A dictionary of keyward args to pass to the method.
-    '''
-    global plugin, runloop
+    """
+
+    global plugin
     callback = lambda *a, **kw: method(plugin, *a, **kw)
-    return runloop.invoke(callback, async = async, kwargs = kwargs)
+
+    return callback(**kwargs)
 
 
 def play_track(**kwargs):
-    ''' Top-level function to retrieve a specific playlist '''
+    """ Top-level function to retrieve a specific playlist """
     return plugin_callback(SpotifyPlugin.play_track, **kwargs)
 
 
 def get_artist_albums(**kwargs):
-    ''' Top-level function to retrieve an artists albums '''
+    """ Top-level function to retrieve an artists albums """
     return plugin_callback(
         SpotifyPlugin.get_artist_albums, async = True, **kwargs)
 
 
 def get_album_tracks(**kwargs):
-    ''' Top-level function to retrieve the tracks in an album '''
+    """ Top-level function to retrieve the tracks in an album """
     return plugin_callback(
         SpotifyPlugin.get_album_tracks, async = True, **kwargs)
 
 
 def get_playlist(**kwargs):
-    ''' Top-level function to retrieve a specific playlist '''
+    """ Top-level function to retrieve a specific playlist """
     return plugin_callback(SpotifyPlugin.get_playlist, **kwargs)
 
 
 def get_playlists(**kwargs):
-    ''' Top-level function to retrieve user playlists '''
+    """ Top-level function to retrieve user playlists """
     return plugin_callback(SpotifyPlugin.get_playlists, **kwargs)
 
 
 def get_starred_tracks(**kwargs):
-    ''' Top-level function to retrieve starred user tracks '''
+    """ Top-level function to retrieve starred user tracks """
     return plugin_callback(SpotifyPlugin.get_starred_tracks, **kwargs)
 
 
 def search(**kwargs):
-    ''' Top-level function to execute a search '''
+    """ Top-level function to execute a search """
     return plugin_callback(SpotifyPlugin.search, async = True, **kwargs)
 
 
 def search_menu(**kwargs):
-    ''' Top-level function to retrieve the search menu '''
+    """ Top-level function to retrieve the search menu """
     return plugin_callback(SpotifyPlugin.search_menu, **kwargs)
 
 
 def main_menu(**kwargs):
-    ''' Top-level function to retrieve the main menu '''
+    """ Top-level function to retrieve the main menu """
     return plugin_callback(SpotifyPlugin.main_menu, **kwargs)
 
 
 def Start():
-    ''' Initialization function '''
+    """ Initialization function """
     Log("Starting Spotify (version %s)", VERSION)
     Plugin.AddPrefixHandler(PREFIX, main_menu, 'Spotify')
     ViewMode.AddModes(Plugin)
+
     ObjectContainer.title1 = 'Spotify'
     ObjectContainer.content = 'Items'
     ObjectContainer.art = R('art-default.png')
@@ -90,6 +78,6 @@ def Start():
 
 
 def ValidatePrefs():
-    ''' Called when the user's prefs are changed '''
+    """ Called when the user's prefs are changed """
     plugin_callback(SpotifyPlugin.preferences_updated)
 
