7895cd61253bdbbe17f23e3233b7faeb97d4fed2
Dean Gardiner <gardiner91@gmail.com>
2014-04-19 14:48:34 +1200

    Updated [settings.py], bumped version to 0.3

--- Contents/Code/settings.py
+++ Contents/Code/settings.py
@@ -1,9 +1,4 @@
-PLUGIN_ID = "com.plexapp.plugins.spotify2"
-VERSION     = 0.2
+PLUGIN_ID   = "com.plexapp.plugins.spotify2"
+VERSION     = 0.3
 PREFIX      = "/music/spotify"
 ROUTEBASE   = PREFIX + '/'
-RESTART_URL = "http://localhost:32400/:/plugins/%s/restart" % PLUGIN_ID
-SERVER_PORT = 32420
-REQUEST_TIMEOUT = 15
-POLL_INTERVAL = 0.1
-POLL_TIMEOUT = 1
