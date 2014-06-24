9d421850fbcc863d62bf3397b32ea083eedf6c53
Dean Gardiner <gardiner91@gmail.com>
2014-06-24 21:04:55 +1200

    Updated spotify.py

--- Contents/Libraries/Shared/spotify/commands/manager.py
+++ Contents/Libraries/Shared/spotify/commands/manager.py
@@ -1,12 +1,23 @@
 from spotify.commands import DoWork, PingFlash2
 
+from pyemitter import Emitter
+import logging
 
-class CommandManager(object):
+log = logging.getLogger(__name__)
+
+
+class CommandManager(Emitter):
     def __init__(self, sp):
+        self.sp = sp
+
         self.handlers = {
             'do_work': DoWork(sp),
             'ping_flash2': PingFlash2(sp)
         }
+
+        # Attach 'error' listeners
+        for command in self.handlers.values():
+            command.on('error', self.on_error)
 
     def process(self, name, *args):
         if name in self.handlers:
@@ -14,3 +25,12 @@ class CommandManager(object):
             return True
 
         return False
+
+    def on_error(self, message):
+        log.warn('Error returned from command handler, disconnecting...')
+
+        # Trigger disconnection
+        self.sp.components.connection.disconnect()
+
+        # Fire 'error'
+        self.emit('error', message)
--- Contents/Libraries/Shared/spotify/commands/work.py
+++ Contents/Libraries/Shared/spotify/commands/work.py
@@ -30,6 +30,9 @@ class DoWork(Command):
         try:
             ctx = execjs.compile(WORK_RUNNER % payload)
             result = ctx.eval('main.run.call(main)')
+        except execjs.RuntimeUnavailable, ex:
+            self.emit('error', 'JavaScript runtime is not available, try installing node.js (http://nodejs.org)')
+            return
         except Exception, ex:
             log.warn('Unable to run work - %s - %s', ex, traceback.format_exc())
             return
